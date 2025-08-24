125846277882e747658bd21e1971a7f6e46bee06
Alexsandro Goes <alexsandrogoes7@gmail.com>
2025-08-24 02:31:34 -0300

    Corrige server.js e adiciona configuração CORS

---
+++ app.html/app.js
@@ -0,0 +1,17 @@
+const lista = document.getElementById("medicamentos-lista");
+
+// Substitua pelo IP do seu computador se estiver usando celular na mesma rede
+const API_URL = "http://10.217.17.166:10000/api/medicamentos";
+
+async function carregarMedicamentos() {
+    const resposta = await fetch(API_URL);
+    const medicamentos = await resposta.json();
+
+    medicamentos.forEach(med => {
+        const li = document.createElement("li");
+        li.textContent = med.nome; // aqui assumimos que o medicamento tem campo "nome"
+        lista.appendChild(li);
+    });
+}
+
+carregarMedicamentos();
